6b453311c99ec0acc5ccc2d6ada7721dbcb311e8
toshbrown <Toshbrown@gmail.com>
2019-02-26 10:41:37 +0000

    Move to new lib-node-databox 0.10.5 (#13)

--- src/main.js
+++ src/main.js
@@ -1,35 +1,32 @@
 /*jshint esversion: 6 */
 const https = require('https');
-const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const monitor = require("os-monitor");
-
+const monitor = require('os-monitor');
 const databox = require('node-databox');
-
-const ws = require('ws');
 
 //
 // Get the needed Environment variables
 //
-const DATABOX_ZMQ_ENDPOINT = process.env.DATABOX_ZMQ_ENDPOINT
+const DATABOX_ZMQ_ENDPOINT = process.env.DATABOX_ZMQ_ENDPOINT || "tcp://127.0.0.1:5555"
+const DATABOX_ARBITER_ENDPOINT = process.env.DATABOX_ARBITER_ENDPOINT || "tcp://127.0.0.1:4444"
 
 //HTTPS certs created by the container mangers for this components HTTPS server.
-credentials = databox.getHttpsCredentials();
+const credentials = databox.GetHttpsCredentials();
 
 var PORT = process.env.port || '8080';
 
 var app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/status", function(req, res) {
-    res.send("active");
+app.get('/status', function (req, res) {
+  res.send('active');
 });
 
-app.get("/ui", function(req, res) {
+app.get('/ui', function (req, res) {
   res.send(`<html>
     <head></head>
     <body>
@@ -40,10 +37,10 @@ app.get("/ui", function(req, res) {
   </html>`);
 });
 
-var vendor = "databox inc";
+var vendor = 'databox inc';
 
 //Blob data sources
-let tsbc = databox.NewTimeSeriesBlobClient(DATABOX_ZMQ_ENDPOINT, false);
+let client = databox.NewStoreClient(DATABOX_ZMQ_ENDPOINT, DATABOX_ARBITER_ENDPOINT, false);
 let loadavg1 = databox.NewDataSourceMetadata();
 loadavg1.Description = 'Databox load average 1 minute';
 loadavg1.ContentType = 'application/json';
@@ -51,7 +48,7 @@ loadavg1.Vendor = 'Databox Inc.';
 loadavg1.Unit = '%';
 loadavg1.DataSourceType = 'loadavg1';
 loadavg1.DataSourceID = 'loadavg1';
-loadavg1.StoreType = 'ts';
+loadavg1.StoreType = 'ts/blob';
 
 let loadavg5 = databox.NewDataSourceMetadata();
 loadavg5.Description = 'Databox load average over 5 minutes';
@@ -60,7 +57,7 @@ loadavg5.Vendor = 'Databox Inc.';
 loadavg5.Unit = '%';
 loadavg5.DataSourceType = 'loadavg5';
 loadavg5.DataSourceID = 'loadavg5';
-loadavg5.StoreType = 'ts';
+loadavg5.StoreType = 'ts/blob';
 
 let loadavg15 = databox.NewDataSourceMetadata();
 loadavg15.Description = 'Databox load average over 15 minutes';
@@ -69,7 +66,7 @@ loadavg15.Vendor = 'Databox Inc.';
 loadavg15.Unit = '%';
 loadavg15.DataSourceType = 'loadavg15';
 loadavg15.DataSourceID = 'loadavg15';
-loadavg15.StoreType = 'ts';
+loadavg15.StoreType = 'ts/blob';
 
 let freemem = databox.NewDataSourceMetadata();
 freemem.Description = 'Databox free memory in bytes';
@@ -78,10 +75,9 @@ freemem.Vendor = 'Databox Inc.';
 freemem.Unit = 'bytes';
 freemem.DataSourceType = 'freemem';
 freemem.DataSourceID = 'freemem';
-freemem.StoreType = 'ts';
+freemem.StoreType = 'ts/blob';
 
 //Structured data sources
-let tsc = databox.NewTimeSeriesClient(DATABOX_ZMQ_ENDPOINT, false);
 let loadavg1Structured = databox.NewDataSourceMetadata();
 loadavg1Structured.Description = 'Databox load average 1 minute structured';
 loadavg1Structured.ContentType = 'application/json';
@@ -100,110 +96,81 @@ freememStructured.DataSourceType = 'freememStructured';
 freememStructured.DataSourceID = 'freememStructured';
 freememStructured.StoreType = 'ts';
 
-console.log("RegisterDatasource:: start")
-tsbc.RegisterDatasource(loadavg1)
-.then((resp)=>{
-  console.log("RegisterDatasource(loadavg1)::", resp)
-  return tsbc.RegisterDatasource(loadavg5);
-})
-.then(()=>{
-  return tsbc.RegisterDatasource(loadavg15);
-})
-.then(()=>{
-  return tsbc.RegisterDatasource(freemem);
-})
-.then(()=>{
-  return tsc.RegisterDatasource(loadavg1Structured);
-})
-.then(()=>{
-  return tsc.RegisterDatasource(freememStructured);
-})
-.then(()=>{
-  console.log("RegisterDatasource:: end")
 
-  monitor.start({ delay: 1000 });
+console.log('RegisterDatasource:: start')
+try {
+  client.RegisterDatasource(loadavg1)
+  client.RegisterDatasource(loadavg5);
+  client.RegisterDatasource(loadavg15);
+  client.RegisterDatasource(freemem);
+  client.RegisterDatasource(loadavg1Structured);
+  client.RegisterDatasource(freememStructured);
+} catch (err) {
+  console.warn(err)
+}
+console.log('RegisterDatasource:: end')
 
-  // define handler that will always fire every cycle
-  monitor.on('monitor', function(event) {
+monitor.start({ delay: 1500 });
 
-    var loadavg1 = event['loadavg'][0];
-    var loadavg5 = event['loadavg'][1];
-    var loadavg15 = event['loadavg'][2];
-    var freemem = event[freemem];
-    console.log(loadavg1);
+// define handler that will always fire every cycle
+monitor.on('monitor', function (event) {
 
-    saveBlob('loadavg1', event['loadavg'][0])
-    .then(()=>{
+  var loadavg1 = event['loadavg'][0];
+  var loadavg5 = event['loadavg'][1];
+  var loadavg15 = event['loadavg'][2];
+  var freemem = event[freemem];
+  console.log(loadavg1);
+
+  saveBlob('loadavg1', event['loadavg'][0])
+    .then(() => {
       return saveBlob('loadavg5', event['loadavg'][1]);
     })
-    .then(()=>{
+    .then(() => {
       return saveBlob('loadavg5', event['loadavg'][1]);
 
     })
-    .then(()=>{
-      return saveBlob('loadavg15',event['loadavg'][2]);
+    .then(() => {
+      return saveBlob('loadavg15', event['loadavg'][2]);
     })
-    .then(()=>{
-      return  saveBlob('freemem', event['freemem']);
+    .then(() => {
+      return saveBlob('freemem', event['freemem']);
 
     })
-    .then(()=>{
+    .then(() => {
       return saveStructured('loadavg1Structured', event['loadavg'][0]);
 
     })
-    .then(()=>{
+    .then(() => {
       return saveStructured('freememStructured', event['freemem']);
     })
-    .then(()=>{
+    .then(() => {
       event = null
       loadavg1 = null
       loadavg5 = null
-      loadavg15 =null
+      loadavg15 = null
       freemem = null
     })
-    .catch((err)=>{
-      console.log("Error writing to store:", error);
+    .catch((err) => {
+      console.log('Error writing to store:', error);
     })
 
-  });
-
-})
-.catch((err)=>{
-  console.log("Error registering data source:" + err);
 });
 
-function saveBlob(datasourceid,data) {
-  let json = {"data":data};
-  console.log("Saving data::", datasourceid, json);
-  return tsbc.Write(datasourceid,json);
+function saveBlob(datasourceid, data) {
+  let json = { 'data': data };
+  console.log('Saving data::', datasourceid, json);
+  return client.TSBlob.Write(datasourceid, json);
 }
 
-function saveStructured(datasourceid,data) {
-  let json = {"value":data};
-  console.log("Saving data::", datasourceid, json);
-  return tsc.Write(datasourceid,json);
+function saveStructured(datasourceid, data) {
+  let json = { 'value': data };
+  console.log('Saving data::', datasourceid, json);
+  return client.TS.Write(datasourceid, json);
 }
 
 server = https.createServer(credentials, app).listen(PORT);
-
-const wss = new ws.Server({ server });
-
 server.listen(PORT, () => {
   console.log(`Server started on port ${server.address().port} :)`);
-});
-
-wss.on('connection', (ws) => {
-
-  //connection is up, let's add a simple simple event
-  ws.on('message', (message) => {
-
-      //log the received message and send it back to the client
-      console.log('received: %s', message);
-      ws.send(`Hello, you sent -> ${message}`);
-  });
-
-  //send immediatly a feedback to the incoming connection
-  ws.send('Hi there, I am a WebSocket echo server');
 });
 
 module.exports = app;
